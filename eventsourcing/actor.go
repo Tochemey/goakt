26b0fd5b1622488f55735b2ff7300bff63f9ed79
Tochemey <tochemey@hotmail.com>
2023-01-02 01:53:56 +0000

    docs: add docstring

--- eventsourcing/actor.go
+++ eventsourcing/actor.go
@@ -17,6 +17,34 @@ import (
 	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
+type Command proto.Message
+type Event proto.Message
+type State proto.Message
+
+// EventSourcedBehavior defines a event sourced behavior when modeling a CQRS Aggregate.
+type EventSourcedBehavior[T State] interface {
+	// Kind defines the kind of actor it is. This in combination with the PersistenceID uniquely identifies the
+	// event sourced actor. For instance Users, Accounts can all be used as kind.
+	Kind() string
+	// PersistenceID defines the id that will be used in the event journal.
+	// This helps track the event sourced actor in the events store.
+	PersistenceID() string
+	// InitialState returns the event sourced actor initial state
+	InitialState() T
+	// HandleCommand helps handle commands received by the event sourced actor. The command handlers define how to handle each incoming command,
+	// which validations must be applied, and finally, which events will be persisted if any. When there is no event to be persisted a nil can
+	// be returned as a no-op. Command handlers are the meat of the event sourced actor.
+	// They encode the business rules of your event sourced actor and act as a guardian of the event sourced actor consistency.
+	// The command handler must first validate that the incoming command can be applied to the current model state.
+	//  Any decision should be solely based on the data passed in the commands and the state of the Aggregate.
+	// In case of successful validation, one or more events expressing the mutations are persisted.
+	// Once the events are persisted, they are applied to the state producing a new valid state.
+	HandleCommand(ctx context.Context, command Command, priorState T) (event Event, err error)
+	// HandleEvent handle events emitted by the command handlers. The event handlers are used to mutate the state of the event sourced actor by applying the events to it.
+	// Event handlers must be pure functions as they will be used when instantiating the event sourced actor and replaying the event journal.
+	HandleEvent(ctx context.Context, event Event, priorState T) (state T, err error)
+}
+
 // eventSourcedActor is an event sourced based actor
 type eventSourcedActor[T State] struct {
 	EventSourcedBehavior[T]
