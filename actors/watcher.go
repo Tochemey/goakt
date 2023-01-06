c0b3d95c91a5674262ce15e9c73a60d8a7eb81c2
Tochemey <tochemey@hotmail.com>
2023-01-06 01:01:54 +0000

    chore: quick cleanup

--- actors/watcher.go
+++ actors/watcher.go
@@ -1,7 +1,7 @@
 package actors
 
 type Watcher struct {
-	Parent  *pid       // the Parent of the actor watching
+	Parent  PID        // the Parent of the actor watching
 	ErrChan chan error // the channel where to pass error message
 	Done    chan Unit
 }
