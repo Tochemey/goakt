baa08c6e5262529d98ac5155081845c286b36e7d
tochemey <tochemey@hotmail.com>
2023-01-18 15:06:56 +0000

    refactor: checkin

--- eventsourcing/projection.go
+++ eventsourcing/projection.go
@@ -1,4 +1,17 @@
 package eventsourcing
 
+import (
+	"context"
+
+	"google.golang.org/protobuf/types/known/anypb"
+)
+
+// ProjectionHandler defines how a projection should handle
+// events consumed from an event store
+type ProjectionHandler interface {
+	Handle(ctx context.Context, persistenceID string, event *anypb.Any, state *anypb.Any) error
+}
+
+// Projection defines the core abstraction of
 type Projection struct {
 }
--- persistence/offset_store.go
+++ persistence/offset_store.go
@@ -1,7 +1,18 @@
 package persistence
 
-import "context"
+import (
+	"context"
 
+	pb "github.com/tochemey/goakt/pb/goakt/v1"
+)
+
+// OffsetStore defines the contract needed to persist persistence entities
+// events offsets
 type OffsetStore interface {
-	WriteOffset(ctx context.Context) error
+	// WriteOffset writes the current offset of the event consumed for a given projection ID
+	WriteOffset(ctx context.Context, offset *pb.Offset) error
+	// GetCurrentOffset returns the current offset of a given projection ID
+	GetCurrentOffset(ctx context.Context, projectionID *ProjectionID) (current *pb.Offset, err error)
+	// GetLatestOffset returns the latest offset a given projection ID
+	GetLatestOffset(ctx context.Context, projectionID *ProjectionID) (latest *pb.Offset, err error)
 }
