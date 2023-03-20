aaf206a62c68b65d893b0141c5a48d219a19a710
tochemey <tochemey@hotmail.com>
2023-03-20 06:02:12 +0000

    feat: mDNS discovery implementation

--- internal/discovery/discovery.go
+++ internal/discovery/discovery.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"fmt"
 
-	"github.com/pkg/errors"
 	goaktpb "github.com/tochemey/goakt/internal/goaktpb/v1"
 )
 
@@ -22,39 +21,6 @@ type Discovery interface {
 	Stop() error
 }
 
-// Meta represents the meta information to pass to the discovery engine
-type Meta map[string]any
-
-// GetString returns the string value of a given key which value is a string
-// If the key value is not a string then an error is return
-func (m Meta) GetString(key string) (string, error) {
-	// let us check whether the given key is in the map
-	val, ok := m[key]
-	if !ok {
-		return "", fmt.Errorf("key=%s not found", key)
-	}
-	// let us check the type of val
-	switch x := val.(type) {
-	case string:
-		return x, nil
-	default:
-		return "", errors.New("the key value is not a string")
-	}
-}
-
-// GetMapString returns the map of string value of a given key which value is a map of string
-// Map of string means that the map key value pair are both string
-func (m Meta) GetMapString(key string) (map[string]string, error) {
-	// let us check whether the given key is in the map
-	val, ok := m[key]
-	if !ok {
-		return nil, fmt.Errorf("key=%s not found", key)
-	}
-	// assert the type of val
-	switch x := val.(type) {
-	case map[string]string:
-		return x, nil
-	default:
-		return nil, errors.New("the key value is not a map[string]string")
-	}
-}
+var (
+	ErrMetaKeyNotFound = func(key string) error { return fmt.Errorf("key=%s not found", key) }
+)
