5ab3e22a8b0a004a29a179499c239e7396008af6
evilolipop <decai.wang@foxmail.com>
2024-05-30 18:22:52 +0800

    feat: add static discovery

---
+++ discovery/static/discovery.go
@@ -0,0 +1,74 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2022-2024 Tochemey
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package static
+
+import (
+	"github.com/tochemey/goakt/v2/discovery"
+)
+
+type Discovery struct {
+	config *Config
+}
+
+// enforce compilation error
+var _ discovery.Provider = &Discovery{}
+
+func NewDiscovery(config *Config) *Discovery {
+	d := &Discovery{
+		config: config,
+	}
+
+	return d
+}
+
+// ID returns the discovery provider identifier
+func (d *Discovery) ID() string {
+	return "static"
+}
+
+// Initialize the discovery provider
+func (d *Discovery) Initialize() error {
+	return nil
+}
+
+// Register registers this node to a service discovery directory.
+func (d *Discovery) Register() error {
+	return nil
+}
+
+// Deregister removes this node from a service discovery directory.
+func (d *Discovery) Deregister() error {
+	return nil
+}
+
+// Close closes the provider
+func (d *Discovery) Close() error {
+	return nil
+}
+
+// DiscoverPeers returns a list of known nodes.
+func (d *Discovery) DiscoverPeers() ([]string, error) {
+	return d.config.Hosts, nil
+}
