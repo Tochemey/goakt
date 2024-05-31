94090c6c21ebbd0f6dd8df82ac4fbe36354537da
Tochemey <tochemey@hotmail.com>
2024-05-31 09:41:31 +0100

    refactor: remove unnecessary features added to example to make them lean

--- examples/actor-cluster/static/service/config.go
+++ examples/actor-cluster/static/service/config.go
@@ -31,7 +31,6 @@ type Config struct {
 	Port            int    `env:"PORT" envDefault:"50051"`
 	ServiceName     string `env:"SERVICE_NAME"`
 	ActorSystemName string `env:"SYSTEM_NAME"`
-	TraceURL        string `env:"TRACE_URL"`
 	GossipPort      int    `env:"GOSSIP_PORT"`
 	PeersPort       int    `env:"PEERS_PORT"`
 	RemotingPort    int    `env:"REMOTING_PORT"`
