5ca540bf0dabe4dbb3cf3141337d4d0bfc223d1c
Tochemey <tochemey@hotmail.com>
2025-04-15 17:49:29 +0100

    test: add missing test

--- actor/actor_system_test.go
+++ actor/actor_system_test.go
@@ -1909,5 +1909,39 @@ func TestActorSystem(t *testing.T) {
 		err = sys.Stop(ctx)
 		assert.NoError(t, err)
 	})
+	t.Run("With cluster enabled and invalid WAL dir", func(t *testing.T) {
+		ctx := context.TODO()
+		nodePorts := dynaport.Get(3)
+		gossipPort := nodePorts[0]
+		clusterPort := nodePorts[1]
+		remotingPort := nodePorts[2]
 
+		logger := log.DiscardLogger
+		host := "127.0.0.1"
+
+		remoteConfig := remote.NewConfig(host, remotingPort)
+		// mock the discovery provider
+		provider := new(testkit.Provider)
+		newActorSystem, err := NewActorSystem(
+			"test",
+			WithPassivationDisabled(),
+			WithLogger(logger),
+			WithRemote(remoteConfig),
+			WithCluster(
+				NewClusterConfig().
+					WithKinds(new(mockActor)).
+					WithPartitionCount(9).
+					WithReplicaCount(1).
+					WithPeersPort(clusterPort).
+					WithMinimumPeersQuorum(1).
+					WithDiscoveryPort(gossipPort).
+					WithWAL("/").
+					WithDiscovery(provider)),
+		)
+		require.NoError(t, err)
+
+		// start the actor system
+		err = newActorSystem.Start(ctx)
+		require.Error(t, err)
+	})
 }
