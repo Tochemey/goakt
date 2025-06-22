1001b40a8fcfd6dac42276fa570804193fb37488
Tochemey <tochemey@hotmail.com>
2025-06-22 12:48:11 +0100

    fix: fix broken tests

--- actor/option.go
+++ actor/option.go
@@ -122,6 +122,7 @@ func WithActorInitTimeout(timeout time.Duration) Option {
 }
 
 // WithPeerStateLoopInterval sets the peer state loop interval
+//
 // Deprecated: This option is deprecated and will be removed in a future release.
 // Use WithClusterStateSyncInterval instead to configure the interval for synchronizing cluster state when
 // setting up a cluster using the ClusterConfig option.
